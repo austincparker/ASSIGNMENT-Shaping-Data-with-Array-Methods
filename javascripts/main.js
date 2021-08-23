bfd3aca0261082cba612fe70ccb75c5b3b646190
Austin Parker <austincparker@gmail.com>
2021-08-23 16:50:49 -0500

    added manufacturing filter

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,9 @@
-import { showCards, showNYBiz } from './components/bizCards.js';
+import { showCards, showNYBiz, showManuCards } from './components/bizCards.js';
 
 const init = () => {
 showCards();
 showNYBiz();
+showManuCards();
 };
 
 init();
